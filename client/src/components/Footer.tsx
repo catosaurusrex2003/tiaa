49dd729dba0d10f697adc91aba3db3c055b4f481
quintessentialdevrev <63969941+quintessentialdevrev@users.noreply.github.com>
2023-11-15 14:32:15 +0530

    Contact Page added

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <footer className="w-screen h-64">
+        <footer className="w-screen h-64 border-2">
             <h1>Footer</h1>
         </footer>
     )
